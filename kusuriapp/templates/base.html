078272a854dac90ba82eca148956a3e44cbcf836
yoshiayu <yoshiayu19771106@gmail.com>
2022-02-26 21:02:06 +0900

    14commit

--- kusuriapp/templates/base.html
+++ kusuriapp/templates/base.html
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="ja">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
--- kusuriapp/urls.py
+++ kusuriapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from kusuriapp import views
+from .views import signupview
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('excel_export/', views.IndexView.excel_export, name='excel_export'),
+    path('signup/', signupview, name='signup'),
 ]
