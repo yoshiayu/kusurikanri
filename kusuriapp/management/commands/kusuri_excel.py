0b6e907837633fa074af769fbb02a2a6a3d3e119
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-15 21:28:14 +0900

    重複したデータは読み込まない２

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -20,21 +20,26 @@ class Command(BaseCommand):
                     uniqued = []
                     for x in iterable:
                         if not x in uniqued:
-                            uniqued.append(x)
-                        # 一行目はヘッダーなのでスキップし、行でループ
-                        for row in ws.iter_rows(min_row=2):
-                            # print(sheet_name)
-                            medicine = CompanyMedicineName()
-                            medicine.initials = str(
-                                Path(f'{folder}/{file}').parent).split('/')[-1][0]
-                            medicine.company_name = sheet_name
-                           # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
-                           # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
 
+                            # 一行目はヘッダーなのでスキップし、行でループ
+                            for row in ws.iter_rows(min_row=2):
+                                # print(sheet_name)
+                                medicine = CompanyMedicineName()
+                                medicine.initials = str(
+                                    Path(f'{folder}/{file}').parent).split('/')[-1][0]
+                                medicine.company_name = sheet_name
+
+                                medicine = [CompanyMedicineName]
+                                medicine_k = []
+                                for i in medicine:
+                                    if i not in medicine_k:
+                                        medicine_k.append(i)
+                           # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
+                           # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                             for cell in row:  # セルでループ
                                 # print(f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
                                 if cell.column == 2:
                                     medicine.medicine_name = cell.value
                                     # print(cell.value)
-
+                            uniqued.append(x)
                             medicine.save()
