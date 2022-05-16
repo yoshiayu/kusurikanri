f2a96a18c0cb249513f8bd892b7db732c4eb8755
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-16 20:23:39 +0900

    重複追加6

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from numpy import iterable
 import openpyxl
 import os
 from pathlib import Path
@@ -16,18 +17,18 @@ class Command(BaseCommand):
                     for sheet_name in wb.sheetnames:  # シートでループ
                         ws = wb[sheet_name]
 
-                def __inter__(medicine):
+                def __inter__(iterable):
                     uniqued = []
-                    for x in CompanyMedicineName:
+                    for x in iterable:
                         if not x in uniqued:
                             uniqued.append(x)
                             # 一行目はヘッダーなのでスキップし、行でループ
                             for row in ws.iter_rows(min_row=2):
                                 # print(sheet_name)
-                                medicine = CompanyMedicineName()
-                                medicine.initials = str(
-                                    Path(f'{folder}/{file}').parent).split('/')[-1][0]
-                                medicine.company_name = sheet_name
+                                #medicine = CompanyMedicineName()
+                                # medicine.initials = str(
+                                #    Path(f'{folder}/{file}').parent).split('/')[-1][0]
+                                #medicine.company_name = sheet_name
 
                                 medicine = [CompanyMedicineName]
                                 medicine_k = []
@@ -36,9 +37,9 @@ class Command(BaseCommand):
                                         medicine_k.append(i)
                            # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                            # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
-                            for cell in row:  # セルでループ
+                            # for cell in row:  # セルでループ
                                 # print(f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
-                                if cell.column == 2:
-                                    medicine.medicine_name = cell.value
+                                # if cell.column == 2:
+                                #    medicine.medicine_name = cell.value
                                     # print(cell.value)
                             medicine.save()
