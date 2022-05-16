acdddb983f95d0e51d16adc7370ce82b4d424ccc
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-16 21:08:35 +0900

    重複追加16

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -19,29 +19,29 @@ class Command(BaseCommand):
                         ws = wb[sheet_name]
 
                 # def __inter__(iterable):
-                #    uniqued = []
-                #    for x in iterable:
-                #        if not x in uniqued:
-                #            uniqued.append(x)
+                    uniqued = []
+                    for x in duplicated:
+                        if not x in uniqued:
+                            uniqued.append(x)
 
-                #            duplicated = [CompanyMedicineName]
-                #            print(duplicated)
+                            duplicated = [CompanyMedicineName]
+                            print(duplicated)
 
-                #            uniqued = set(duplicated)
-                #            print(uniqued)
-                        # 一行目はヘッダーなのでスキップし、行でループ
-                        for row in ws.iter_rows(min_row=2):
-                            print(sheet_name)
-                        medicine = CompanyMedicineName()
-                        medicine.initials = str(
-                            Path(f'{folder}/{file}').parent).split('/')[-1][0]
-                        medicine.company_name = sheet_name
+                            uniqued = set(duplicated)
+                            print(uniqued)
+                            # 一行目はヘッダーなのでスキップし、行でループ
+                            for row in ws.iter_rows(min_row=2):
+                                print(sheet_name)
+                            medicine = CompanyMedicineName()
+                            medicine.initials = str(
+                                Path(f'{folder}/{file}').parent).split('/')[-1][0]
+                            medicine.company_name = sheet_name
 
-                        medicine = [CompanyMedicineName]
-                        medicine_k = []
-                        for i in medicine:
-                            if i not in medicine_k:
-                                medicine_k.append(i)
+                            medicine = [CompanyMedicineName]
+                            medicine_k = []
+                            for i in medicine:
+                                if i not in medicine_k:
+                                    medicine_k.append(i)
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                             for cell in row:  # セルでループ
