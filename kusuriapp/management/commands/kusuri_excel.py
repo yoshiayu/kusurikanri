387b2d2096af3c96be2bffc8b7cca3ad3afad288
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-17 20:46:03 +0900

    重複追加33

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -32,15 +32,15 @@ class Command(BaseCommand):
                                         continue
                                 else:
                                     ws.delete_row(i)
-                            wb.save()
+                            wb.save(f'{folder}/{file}/1')
 
                         # 一行目はヘッダーなのでスキップし、行でループ
-                        for row in ws.iter_rows(min_row=2):
-                            print(sheet_name)
-                            medicine = CompanyMedicineName()
-                            medicine.initials = str(
-                                Path(f'{folder}/{file}').parent).split('/')[-1][0]
-                            medicine.company_name = sheet_name
+                        # for row in ws.iter_rows(min_row=2):
+                        #    print(sheet_name)
+                        #    medicine = CompanyMedicineName()
+                        #    medicine.initials = str(
+                        #        Path(f'{folder}/{file}').parent).split('/')[-1][0]
+                        #    medicine.company_name = sheet_name
 
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
