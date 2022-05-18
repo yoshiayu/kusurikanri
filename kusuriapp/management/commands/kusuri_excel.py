0267403b90baeacd85cf4cbcd241064e99c2b90e
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-18 20:27:08 +0900

    重複追加37

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -10,47 +10,46 @@ from kusuriapp.models import CompanyMedicineName
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        # def __inter__(file):
 
         for folder, subFolder, files in os.walk('static'):
             for file in files:
                 if file.endswith('.xlsx'):
                     wb = openpyxl.load_workbook(f'{folder}/{file}')
+                    uniqued = []
+                    for x in cell.value:
+                        if not x in uniqued:
+                            uniqued.append(x)
+
+                    medicine = [cell.value]
+                    medicine_k = []
+                    for i in medicine:
+                        if i not in medicine_k:
+                            medicine_k.append(i)
+
+                    duplicated = [cell.value]
+                    print(duplicated)
+
+                    uniqued = set(duplicated)
+                    print(uniqued)
+
                     for sheet_name in wb.sheetnames:  # シートでループ
                         ws = wb[sheet_name]
 
-                        medicinename = []
-
-                        for Q in range(ws.max_row + 1):
-                            if Q == 0:
-                                continue
-                            list = ws.cell(i, 1).value
-                            list_Num = Q
-
-                            for i in reversed(range(ws.max_row + 1)):
-                                if i == 0:
-                                    break
-                                if ws.cell(i, 1).value == list:
-                                    if i == Q:
-                                        continue
-                                else:
-                                    ws.delete_row(i)
-                            wb.save(f'{folder}/{file}/1')
-
                         # 一行目はヘッダーなのでスキップし、行でループ
-                        # for row in ws.iter_rows(min_row=2):
-                        #    print(sheet_name)
-                        #    medicine = CompanyMedicineName()
-                        #    medicine.initials = str(
-                        #        Path(f'{folder}/{file}').parent).split('/')[-1][0]
-                        #    medicine.company_name = sheet_name
+                        for row in ws.iter_rows(min_row=2):
+                            print(sheet_name)
+                            medicine = CompanyMedicineName()
+                            medicine.initials = str(
+                                Path(f'{folder}/{file}').parent).split('/')[-1][0]
+                            medicine.company_name = sheet_name
 
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
-                        #    for cell in row:  # セルでループ
-                        #        print(
-                        #            f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
-                        #        if cell.column == 2:
-                        #            medicine.medicine_name = cell.value
-                        #        print(cell.value)
-
-                        #    medicine.save()
+                            for cell in row:  # セルでループ
+                                print(
+                                    f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
+                                if cell.column == 2:
+                                    medicine.medicine_name = cell.value
+                                print(cell.value)
+                            medicine.save()
