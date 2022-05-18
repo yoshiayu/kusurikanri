6ac5a2b1864b148e91588ffcc9a88bd35687b70b
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-18 21:32:26 +0900

    重複追加44

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -5,6 +5,7 @@ import openpyxl
 import os
 from pathlib import Path
 from kusuriapp.models import CompanyMedicineName
+import pandas as pd
 
 
 class Command(BaseCommand):
@@ -36,28 +37,20 @@ class Command(BaseCommand):
                                 if cell.column == 2:
                                     medicine.medicine_name = cell.value
                                 print(cell.value)
+
+                                df = pd.read_excel(".xlsx")
+                                del_list = df.loc[:, cell.value].drop_duplicates(
+                                    keep='last').dropna().to_list()
+
                                 medicine.save()
 
-                                cell.value = []
+                                medicine = [cell.value]
+                                medicine_k = []
+                                for i in medicine:
+                                    if i not in medicine_k:
+                                        medicine_k.append(i)
 
-                                for Q in range(ws.max_row + 1):
-                                    if Q == 0:
-                                        continue
-
-                                    list = ws.cell(Q, 1).value
-
-                                    list_Num = Q
-
-                                    for i in reversed(range(ws.max_row + 1)):
-                                        if i == 0:
-                                            break
-
-                                        if i == Q:
-                                            continue
-                                        else:
-                                            ws.delete_rows(i)
-                                wb.save(f'{folder}/{file}/1')
-
+                            # 以下ゴミ
                                 #uniqued = []
                                 # for x in cell:
                                 #    if not x in uniqued:
@@ -69,9 +62,24 @@ class Command(BaseCommand):
                                 #uniqued = set(duplicated)
                                 # print(uniqued)
 
-                                #medicine = [cell.value]
-                                #medicine_k = []
-                                # for i in medicine:
-                                #    if i not in medicine_k:
-                                #        medicine_k.append(i)
                             # medicine.save()
+
+                            #cell.value = []
+
+                            #    for Q in range(ws.max_row + 1):
+                            #        if Q == 0:
+                            #            continue
+
+                            #        list = ws.cell(Q, 1).value
+
+                            #        list_Num = Q
+
+                            #        for i in reversed(range(ws.max_row + 1)):
+                            #            if i == 0:
+                            #                break
+
+                            #            if i == Q:
+                            #                continue
+                            #            else:
+                            #                ws.delete_rows(i)
+                            #    wb.save(f'{folder}/{file}/1')
