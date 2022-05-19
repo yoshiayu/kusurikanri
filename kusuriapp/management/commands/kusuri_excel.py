5fe3706acbbc026fa438b6062c2574f1ea95cdfe
yoshiayu <yoshi@yoshinoMacBook-Pro.local>
2022-05-19 20:36:46 +0900

    重複追加60

--- kusuriapp/management/commands/kusuri_excel.py
+++ kusuriapp/management/commands/kusuri_excel.py
@@ -31,18 +31,18 @@ class Command(BaseCommand):
 
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
                        # print(str(Path(f'{folder}/{file}').parent).split('/')[-1][0])
-                            medicine.medicine_name = []
+
+                            for cell in row:  # セルでループ
+
+                                print(
+                                    f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
+                                medicine.medicine_name = []
                             for i in range(1, ws.max_row):
                                 if ws.cell(i, 1).value != ws.cell(i-1, 1).value:
                                     medicine.medicine_name.append(
                                         ws.cell(i, 1).value)
                                     medicine.medicine_name = list(
                                         filter(None, cell.value))
-
-                            for cell in row:  # セルでループ
-
-                                print(
-                                    f'row: {cell.row}, column: {cell.column}, value: {cell.value}')
                                 if cell.column == 2:
 
                                     medicine.medicine_name = cell.value
